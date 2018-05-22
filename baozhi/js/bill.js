725576de07bce424ccf552a5be4cdb246113d1a1
TCPSoftware <1691957831@qq.com>
2018-05-22 12:17:22 +0800

    update

--- baozhi/js/bill.js
+++ baozhi/js/bill.js
@@ -1,22 +1,62 @@
 var bill = [
 	{
+		// OK
 		type: "add_user",
-		class: 1916,
+		class: 1999,
 		name: "TCP",
-		money: "0",
-		bio: 1
+		money: 10,
+		bio: 1,
+		year: 2018,
+		month: 05,
+		date: 01
 	},
 	{
 		type: "charge",
-		class: 1916,
+		class: 1999,
 		name: "TCP",
-		money: 8
+		money: 8,
+		year: 2018,
+		month: 05,
+		date: 02
 	},
-	{},
-	{},
-	{},
-	{},
-	{}
+	{
+		// OK
+		type: "sell_newspaper",
+		month: 1,
+		money: -8,
+		year: 2018,
+		month: 05,
+		date: 03
+	},
+	{
+		// OK
+		type: "sell",
+		month: 2,
+		money: -2,
+		year: 2018,
+		month: 05,
+		date: 04
+	},
+	{
+		// OK
+		type: "add",
+		name: "TCL",
+		class: 1916,
+		money: 100,
+		bio: 1,
+		year: 2018,
+		month: 05,
+		date: 05
+	},
+	{
+		// OK
+		type: "sell",
+		month: 2,
+		money: -8,
+		year: 2018,
+		month: 05,
+		date: 06
+	}
 ];
 var the_users = [
 	{
@@ -31,66 +71,6 @@ var the_users = [
 	},
 	{
 		class: 1917,
-		name: "TCP",
-		bio: 1,
-		rest1: 10,
-		charge: 2,
-		rest2: 0,
-		cost: -1,
-		rest3: 0,
-	},
-	{
-		class: 1918,
-		name: "TCP",
-		bio: 1,
-		rest1: 10,
-		charge: 2,
-		rest2: 0,
-		cost: -1,
-		rest3: 0,
-	},
-	{
-		class: 1919,
-		name: "TCP",
-		bio: 1,
-		rest1: 10,
-		charge: 2,
-		rest2: 0,
-		cost: -1,
-		rest3: 0,
-	},
-	{
-		class: 1920,
-		name: "TCP",
-		bio: 1,
-		rest1: 10,
-		charge: 2,
-		rest2: 0,
-		cost: -1,
-		rest3: 0,
-	},
-	{
-		class: 1921,
-		name: "TCP",
-		bio: 1,
-		rest1: 10,
-		charge: 2,
-		rest2: 0,
-		cost: -1,
-		rest3: 0,
-	},
-	{
-		class: 1922,
-		name: "TCP",
-		bio: 1,
-		rest1: 10,
-		charge: 2,
-		rest2: 0,
-		cost: -1,
-		rest3: 0,
-	},
-	{
-		class: 1923,
 		name: "TCP",
 		bio: 1,
 		rest1: 10,
